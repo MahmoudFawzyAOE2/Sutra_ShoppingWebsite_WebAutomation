176905620e3391894d565d0d067f568c978d09bd
Mohanad Ehab <139120908+Mohanadehab@users.noreply.github.com>
2024-10-09 19:34:31 +0300

    Update MainRegisterTests.java

--- src/test/java/TestSuite/Register/MainRegisterTests.java
+++ src/test/java/TestSuite/Register/MainRegisterTests.java
@@ -7,49 +7,51 @@ import jdk.jfr.Description;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
-
 public class MainRegisterTests extends BaseTest {
 
-    private RegisterPage registerPage;
-    private BasicMethods basicMethods;
+    RegisterPage registerPage;
 
     @BeforeMethod
     public void setUp() {
-        // Connect WebDriver from BaseTest with WebDriver from SearchMethods
-        basicMethods = new BasicMethods(driver);
+        // Initialize BasicMethods and RegisterPage
+        BasicMethods basicMethods = new BasicMethods(driver);
         registerPage = new RegisterPage(driver);
     }
+    /*private void validateEmailField(String firstName, String lastName, String email, String password, String expectedMessage) throws InterruptedException {
+        // Open registration page
+        registerPage.openRegistrationPageInNewTab();
+        // Register user
+        registerPage.registerUser(firstName, lastName, email, password);
+        // Validate the email field
+        boolean isEmailFieldInvalid = registerPage.isFieldValid("RegisterForm-email");
 
+        // Print validation result
+        if (!isEmailFieldInvalid) {
+            System.out.println("Validation message appeared: " + expectedMessage);
+        } else {
+            System.out.println("No validation message, the field is valid.");
+        }
+    }
+*/
     @Test(priority = 1)
     @Description("Check availability of page")
-    public void smokeTest() {
-        // URL 1
-        driver.get(RegisterPage.fakeEmailWebsiteURL);
+    public void smokeTest() throws InterruptedException {
+        // Generate a fake email using the RegisterPage class method
+        String fakeEmail = registerPage.getFakeEmail();
 
-        List<WebElement> emailButtons = driver.findElements(By.cssSelector("button.fem.btn.btn-su2.waves-effect.waves-light.waves-raised"));
+        // Open the registration page in a new tab
+        registerPage.openRegistrationPageInNewTab();
 
-        // Click generate Email button
-        emailButtons.get(1).click();
-
-        WebElement emailParagraph = driver.findElement(By.cssSelector("p b span#email_ch_text"));
-        String email = emailParagraph.getText();
-
-        // Open a new tab
-        ((JavascriptExecutor) driver).executeScript("window.open()");
-
-        // Switch to the new tab
-        String newTabHandle = driver.getWindowHandles().stream().filter(handle -> !handle.equals(driver.getWindowHandle())).findFirst().get();
-        driver.switchTo().window(newTabHandle);
-
-        // URL 2
-        driver.get(RegisterPage.registerURL);
-
-        List<WebElement> registerFields  = driver.findElements(By.cssSelector("input.form-input.form-input-placeholder"));
+        // Locate the registration input fields
+        List<WebElement> registerFields = driver.findElements(By.cssSelector("input.form-input.form-input-placeholder"));
         /*
         0: First Name
         1: Last Name
@@ -57,15 +59,77 @@ public class MainRegisterTests extends BaseTest {
         3: Password
         */
 
-        registerFields.get(2).sendKeys(email);
+        registerPage.registerUser("Mohanad", "Ehab", fakeEmail, "123456789");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlToBe("https://sutrastores.com/en/challenge"));
+        String actualURL = driver.getCurrentUrl();
+        Assert.assertEquals(actualURL, "https://sutrastores.com/en/challenge", "Registration did not redirect to the expected page.");
 
-        /*
-        WebElement emailField  = driver.findElement(By.cssSelector("input#RegisterForm-email"));
-        emailField.sendKeys(email);
-        */
+    }
+
+    @Test(priority = 2)
+    @Description("Verify Popup Message Appearance for Invalid Email(Empty)")
+    public void testEmptyEmail() throws InterruptedException {
+
+        registerPage.openRegistrationPageInNewTab();
 
 
+        registerPage.registerUser("Test", "User", "", "12345");
+
+        //WebElement CaptchaButton = driver.findElement(By.id("checkbox"));
+       // WebElement SubmitButton = driver.findElement(By.xpath("//input[@value='Submit']"));
+        //CaptchaButton.click();
+        //SubmitButton.click();
+        //String actualURL = driver.getCurrentUrl();
+        WebElement emailField = driver.findElement(By.id("RegisterForm-email"));
+
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        Boolean isEmailFieldInvalid = (Boolean) js.executeScript("return arguments[0].checkValidity();", emailField);
+
+        if (Boolean.FALSE.equals(isEmailFieldInvalid)) {
+            System.out.println("Validation message appeared: Please fill out this field.");
+        } else {
+            System.out.println("No validation message, the field is valid.");
+        }
 
 
     }
-}
+
+    @Test(priority = 3)
+    @Description("Verify Popup Message Appearance for Invalid Password(Empty)")
+    public void testEmptyPassword() throws InterruptedException {
+        // Generate a fake email
+        String fakeEmail = registerPage.getFakeEmail();
+        // Open the registration page
+        registerPage.openRegistrationPageInNewTab();
+        WebElement passwordField = driver.findElement(By.id("RegisterForm-password"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        Boolean isEmailFieldInvalid = (Boolean) js.executeScript("return arguments[0].checkValidity();", passwordField);
+
+        if (Boolean.FALSE.equals(isEmailFieldInvalid)) {
+            System.out.println("Validation message appeared: Please fill out this field.");
+        } else {
+            System.out.println("No validation message, the field is valid.");
+        }
+
+    }
+    @Test(priority = 4)
+    @Description("Verify Popup Message Appearance for Invalid Email (Without '@')")
+    public void testEmailWithoutAt() throws InterruptedException {
+        registerPage.validateEmailField("Test", "User", "test.user", "12345", "Please include an '@' in the email address. 'test.user' is missing an '@'.");
+    }
+    @Test(priority = 5)
+    @Description("Verify Popup Message Appearance for Invalid Email (With nothing after '.')")
+    public void testEmailWithNothingAfterDot() throws InterruptedException {
+        registerPage.validateEmailField("Test", "User", "test@user.", "12345", "'.' is used at wrong position in test@user.");
+    }
+    @Test(priority = 3 )
+    @Description("Verify Popup Message Appearance for Invalid Email(With anything after .)")
+    public void testEmailWithNothingAfterAt() throws InterruptedException {
+        registerPage.validateEmailField("Test", "User", "test@user.", "12345", "'.' is used at wrong position in test@user.");
+
+    }
+
+    }
+
